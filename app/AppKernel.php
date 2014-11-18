ea254d9b354177c14d5b9dc0889ccac19916c48d
Robin-LD <robinld95@gmail.com>
2014-11-18 11:54:33 +0100

    Fullcalendar ressources | Add following bundles: FOSJsRoutingBundle ; adesignsCalendarBundle. Both register in AppKernel.php . Placed in web/bundles/ and vendor/

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new ADesigns\CalendarBundle\ADesignsCalendarBundle(),
             new Pico\AssetsBundle\PicoAssetsBundle(),
             new Pico\UserManagementBundle\UserManagementBundle(),
             new Pico\NotificationBundle\PicoNotificationBundle(),
