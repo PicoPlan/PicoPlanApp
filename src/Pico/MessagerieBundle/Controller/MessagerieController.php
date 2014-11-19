3ee4be5566bf5da6f36816c30779746568b7d656
Punkoleo <punkoleo@punkoleo.Marvin>
2014-11-19 14:56:32 -0500

    Message | (en cours) fonction d'affichage

--- src/Pico/MessagerieBundle/Controller/MessagerieController.php
+++ src/Pico/MessagerieBundle/Controller/MessagerieController.php
@@ -9,19 +9,28 @@ use Pico\MessagerieBundle\Entity\messages;
 use Symfony\Component\HttpFoundation\Response;
 class MessagerieController extends Controller
 {
+	private $EntityManager;
+	private $User;
 	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->EntityManager = $this->getDoctrine()->getManager();
+		$this->User = new User();
+		
+	}
 	public function addAction()
 	{
 		// Pour les tests, on crée un user
-		$User = new User();
-		$User->setLogin('punkoTest');
-		$User->setMdp('test');
-		$User->setSecurityLvl("99");
-		$User->setFirstName('Moi');
-		$User->setLastName('Leo');
-		$User->setEmail('Punkoleo@gmail.com');
-		$User->setPhone('0123456789');
-		$User->setBirthDate('un bug ici ;)');
+		$this->User = new User();
+		$this->User->setLogin('punkoTest');
+		$this->User->setMdp('test');
+		$this->User->setSecurityLvl("99");
+		$this->User->setFirstName('Moi');
+		$this->User->setLastName('Leo');
+		$this->User->setEmail('Punkoleo@gmail.com');
+		$this->User->setPhone('0123456789');
+		$this->User->setBirthDate('un bug ici ;)');
 	
 		// Puis deux messages
 		$Message1 = new messages();
@@ -39,24 +48,47 @@ class MessagerieController extends Controller
 		$Message2->setTopVu(0);
 	
 		// On lie les messages à l'user
-		$Message1->setUser($User);
-		$Message2->setUser($User);
-	
-		// On récupère l'EntityManager
-		$EntityManager = $this->getDoctrine()->getManager();
+		$Message1->setUser($this->User);
+		$Message2->setUser($this->User);
 	
 		// Étape 1 : On « persiste » l'entité
-		$EntityManager->persist($User);
+		$this->EntityManager->persist($this->User);
 	
 		// Étape 1 bis : pour cette relation pas de cascade lorsqu'on persiste Advert, car la relation est
 		// définie dans l'entité Application et non Advert. On doit donc tout persister à la main ici.
-		$EntityManager->persist($Message1);
-		$EntityManager->persist($Message2);
+		$this->EntityManager->persist($Message1);
+		$this->EntityManager->persist($Message2);
 	
 		// Étape 2 : On « flush » tout ce qui a été persisté avant
-		$EntityManager->flush();
+		$this->EntityManager->flush();
 	
 		return new Response('Ok');
+	}
+	
+	public function viewAction($id)
+	{
+		$em = $this->getDoctrine()->getManager();
+	
+		// On récupère l'annonce $id
+		$advert = $em
+		->getRepository('OCPlatformBundle:Advert')
+		->find($id)
+		;
+	
+		if (null === $advert) {
+			throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+		}
+	
+		// On récupère la liste des candidatures de cette annonce
+		$listApplications = $em
+		->getRepository('OCPlatformBundle:Application')
+		->findBy(array('advert' => $advert))
+		;
+	
+		return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+				'advert'           => $advert,
+				'listApplications' => $listApplications
+		));
 	}
 	
 	/**
