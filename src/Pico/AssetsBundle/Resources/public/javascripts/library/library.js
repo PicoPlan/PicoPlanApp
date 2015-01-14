a8f78ea7fdeb7a0c9292587c1c1f17c9c16591cf
Rolandkuku <roland.kuku@gmail.com>
2015-01-14 12:12:37 +0100

    assets: enabling js

---
+++ src/Pico/AssetsBundle/Resources/public/javascripts/library/library.js
@@ -0,0 +1,16 @@
+
+
+// Alert Info, displays alert info if exists
+
+$(function() {
+    var alert_info = '<?php echo $alert_info ?>';
+
+    alert(alert_info);    
+
+    // if(alert_info !== 'undefined'){
+    //     alert("pas d'info");
+    // }
+    // else {
+    //     alert(alert_info);
+    // }
+});
--- src/Pico/UserBundle/Controller/UserController.php
+++ src/Pico/UserBundle/Controller/UserController.php
@@ -13,4 +13,18 @@ class UserController extends Controller
        	'title' => 'PicoPlan'
         ));
     }
+
+    public function confirmedAction() {
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        var_dump('uech');
+
+        return $this->render('::base.html.twig', array(
+            'user' => $user,
+        ));
+    }
 }
+
