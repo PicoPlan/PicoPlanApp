31286cc77eebaed365e936c36656c2aae0ba7aec
Rolandkuku <roland.kuku@gmail.com>
2015-02-02 20:19:20 +0100

    UserBundle: Fixing dep injection due to Abde shit and som proper inheritances

--- src/Pico/UserBundle/Controller/RegistrationController.php
+++ src/Pico/UserBundle/Controller/RegistrationController.php
@@ -16,19 +16,7 @@ use FOS\UserBundle\Model\UserInterface;
 
 class RegistrationController extends FOSRegistrationController
 {
-    public function confirmedAction() {
-        $user = $this->getUser();
-        if (!is_object($user) || !$user instanceof UserInterface) {
-            throw new AccessDeniedException('This user does not have access to this section.');
-        }
-
-        $alert_info = "Votre compte est bien enregistré !";
-        $alert_class = "success";
-
-        return $this->render('::base.html.twig', array(
-            'user' => $user,
-            'alert_info' => $alert_info,
-            'alert_class' => $alert_class,
-        ));
+    public function getParent() {
+        return "FOSUserBundle";
     }
 }
