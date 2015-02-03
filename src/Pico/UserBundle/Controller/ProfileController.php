5bfd7e7a5b20f988e516eacc1105ca329a455236
Rolandkuku <roland.kuku@gmail.com>
2015-02-03 16:51:00 +0100

    UserBundle|edit: easy redirect

--- src/Pico/UserBundle/Controller/ProfileController.php
+++ src/Pico/UserBundle/Controller/ProfileController.php
@@ -23,6 +23,40 @@ class ProfileController extends BaseController {
         return 'FOSUserBundle';
     }
 
+    public function showAction() {
+        $user = $this->get('security.context')
+            ->getToken()
+            ->getUser();
+
+        $data = array(
+            'username' => array(
+                'content' => $user->getUsername(),
+                'title' => 'Pseudo',
+                'icon' => 'glyphicon-star'),
+            'last_name' => array(
+                'content' => $user->getLastName(),
+                'title' => 'Nom',
+                'icon' => 'glyphicon-user'),
+            'first_name' => array(
+                'content' => $user->getFirstName(),
+                'title' => 'Prénom',
+                'icon' => 'glyphicon-user'),
+            'email' => array(
+                'content' => $user->getEmail(),
+                'title' => 'Email',
+                'icon' => 'glyphicon-envelope'),
+            'phone' => array(
+                'content' => $user->getPhone(),
+                'title' => 'Téléphone',
+                'icon' => 'glyphicon-phone'),
+        );
+
+
+        return $this->render('PicoUserBundle:User:show.html.twig', array(
+            'data' => $data,
+        ));
+    }
+
     public function editAction(Request $request) {
         # return parent edit action
         # no special stuff here
