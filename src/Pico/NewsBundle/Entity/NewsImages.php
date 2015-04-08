132a400a02364b4ebc6e15b1d00085ee5b2b1b08
Yaiba782 <yaiba782@gmail.com>
2015-04-08 21:17:54 +0200

    NewsBundle || Adding a new Image Class for News Images

---
+++ src/Pico/NewsBundle/Entity/NewsImages.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Pico\NewsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * NewsImages
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Pico\NewsBundle\Entity\NewsImagesRepository")
+ */
+class NewsImages
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="news", type="integer")
+     */
+    private $news;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="path", type="string", length=255)
+     */
+    private $path;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="picture", type="string", length=255)
+     */
+    private $picture;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="isActive", type="boolean")
+     */
+    private $isActive;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set news
+     *
+     * @param integer $news
+     * @return NewsImages
+     */
+    public function setNews($news)
+    {
+        $this->news = $news;
+
+        return $this;
+    }
+
+    /**
+     * Get news
+     *
+     * @return integer 
+     */
+    public function getNews()
+    {
+        return $this->news;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     * @return NewsImages
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string 
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * Set picture
+     *
+     * @param string $picture
+     * @return NewsImages
+     */
+    public function setPicture($picture)
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    /**
+     * Get picture
+     *
+     * @return string 
+     */
+    public function getPicture()
+    {
+        return $this->picture;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     * @return NewsImages
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean 
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+}
